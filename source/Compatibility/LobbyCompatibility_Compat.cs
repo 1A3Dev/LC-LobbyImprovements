34cecf642675cbc84c017189f03d3b68d5c70209
1A3 <mcplayer1a3@gmail.com>
2024-11-04 19:00:07 +0000

    this should fix it

--- source/Compatibility/LobbyCompatibility_Compat.cs
+++ source/Compatibility/LobbyCompatibility_Compat.cs
@@ -49,10 +49,10 @@ namespace LobbyImprovements.Compatibility
             moddedLobbySlot.Setup(lobbySlot);
         }
         
-        [HarmonyPatch(typeof(LobbyHelper), "GetLobbyDiff", new Type[] { typeof(Lobby?), typeof(string) })]
+        [HarmonyPatch(typeof(LobbyHelper), "GetLobbyDiff", new Type[] { typeof(Lobby), typeof(string) })]
         [HarmonyPrefix]
         [HarmonyWrapSafe]
-        private static void GetLobbyDiff(ref string? lobbyPluginString)
+        private static void GetLobbyDiff(ref string lobbyPluginString)
         {
             if (GameNetworkManager.Instance && GameNetworkManager.Instance.disableSteam && lobbyPluginString.IsNullOrWhiteSpace() && LANLobbyManager_InGame.currentLobby != null)
             {
